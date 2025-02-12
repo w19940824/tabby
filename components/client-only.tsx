8a8731c3b38bd6a54939ca5524a009d964369574
w19940824 <374609786@qq.com>
2025-02-12 15:21:23 +0800

    Update client-only.tsx

--- components/client-only.tsx
+++ components/client-only.tsx
@@ -2,7 +2,7 @@ import { useHydrated } from '@/lib/hooks/use-hydration'
 
 type Props = {
   children: React.ReactNode
-  fallback?: React.ReactNode
+  fallback?: React.ReactNode。
 }
 
 export function ClientOnly({ children, fallback = null }: Props) {
