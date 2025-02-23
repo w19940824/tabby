eb8f695b0bd58d2518148acd85e184329e90da0c
w19940824 <374609786@qq.com>
2025-02-23 09:43:37 +0800

    Add files via upload

--- layout.tsx
+++ layout.tsx
@@ -1,51 +1,25 @@
 import { Metadata } from 'next'
 
-import { Toaster } from '@/components/ui/sonner'
+import { LicenseBanner } from '@/components/license-banner'
 
-import '@/app/globals.css'
-
-import { fontMono, fontMontserrat, fontSans } from '@/lib/fonts'
-import { cn } from '@/lib/utils'
-import { Providers } from '@/components/providers'
-import { TailwindIndicator } from '@/components/tailwind-indicator'
-
-import Main from './components/main'
+import Layout from './components/dashboard-layout'
 
 export const metadata: Metadata = {
   title: {
-    default: 'Tabby',
+    default: 'Dashboard',
     template: `Tabby - %s`
-  },
-  description: 'Tabby, an opensource, self-hosted AI coding assistant.',
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' }
-  ],
-  icons: '/favicon.ico'
+  }
 }
 
-interface RootLayoutProps {
+export default function DashboardLayout({
+  children
+}: {
   children: React.ReactNode
-}
-
-export default function RootLayout({ children }: RootLayoutProps) {
+}) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          'bg-transparent font-sans antialiased',
-          fontSans.variable,
-          fontMono.variable,
-          fontMontserrat.variable
-        )}
-      >
-        <Providers attribute="class" defaultTheme="system" enableSystem>
-          <Main>{children}</Main>
-          <Toaster richColors closeButton />
-          <TailwindIndicator />
-        </Providers>
-      </body>
-    </html>
+    <>
+      <LicenseBanner />
+      <Layout>{children}</Layout>
+    </>
   )
 }
