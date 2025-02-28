e0e6126f5b68d96808839eb7068c723435a9f4d6
w19940824 <374609786@qq.com>
2025-02-28 09:02:02 +0800

    Add files via upload

---
+++ service/background_job/mod.rs
@@ -0,0 +1,165 @@
+mod db;
+mod git;
+mod helper;
+mod index_garbage_collection;
+mod license_check;
+mod third_party_integration;
+mod web_crawler;
+
+use std::{str::FromStr, sync::Arc};
+
+use cron::Schedule;
+use futures::StreamExt;
+use git::SchedulerGitJob;
+use helper::{CronStream, Job, JobLogger};
+use index_garbage_collection::IndexGarbageCollection;
+use juniper::ID;
+use license_check::LicenseCheckJob;
+use serde::{Deserialize, Serialize};
+use tabby_common::config::CodeRepository;
+use tabby_db::DbConn;
+use tabby_inference::Embedding;
+use tabby_schema::{
+    context::ContextService,
+    integration::IntegrationService,
+    job::JobService,
+    license::LicenseService,
+    notification::NotificationService,
+    repository::{GitRepositoryService, RepositoryService, ThirdPartyRepositoryService},
+};
+use third_party_integration::SchedulerGithubGitlabJob;
+use tracing::{debug, warn};
+pub use web_crawler::WebCrawlerJob;
+
+use self::{db::DbMaintainanceJob, third_party_integration::SyncIntegrationJob};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum BackgroundJobEvent {
+    SchedulerGitRepository(CodeRepository),
+    SchedulerGithubGitlabRepository(ID),
+    SyncThirdPartyRepositories(ID),
+    WebCrawler(WebCrawlerJob),
+    IndexGarbageCollection,
+}
+
+impl BackgroundJobEvent {
+    pub fn name(&self) -> &'static str {
+        match self {
+            BackgroundJobEvent::SchedulerGitRepository(_) => SchedulerGitJob::NAME,
+            BackgroundJobEvent::SchedulerGithubGitlabRepository(_) => {
+                SchedulerGithubGitlabJob::NAME
+            }
+            BackgroundJobEvent::SyncThirdPartyRepositories(_) => SyncIntegrationJob::NAME,
+            BackgroundJobEvent::WebCrawler(_) => WebCrawlerJob::NAME,
+            BackgroundJobEvent::IndexGarbageCollection => IndexGarbageCollection::NAME,
+        }
+    }
+
+    pub fn to_command(&self) -> String {
+        serde_json::to_string(self).expect("Failed to serialize background job event")
+    }
+}
+
+pub async fn start(
+    db: DbConn,
+    job_service: Arc<dyn JobService>,
+    git_repository_service: Arc<dyn GitRepositoryService>,
+    third_party_repository_service: Arc<dyn ThirdPartyRepositoryService>,
+    integration_service: Arc<dyn IntegrationService>,
+    repository_service: Arc<dyn RepositoryService>,
+    context_service: Arc<dyn ContextService>,
+    license_service: Arc<dyn LicenseService>,
+    notification_service: Arc<dyn NotificationService>,
+    embedding: Arc<dyn Embedding>,
+) {
+    let mut hourly =
+        CronStream::new(Schedule::from_str("@hourly").expect("Invalid cron expression"))
+            .into_stream();
+
+    let mut daily = CronStream::new(Schedule::from_str("@daily").expect("Invalid cron expression"))
+        .into_stream();
+
+    tokio::spawn(async move {
+        loop {
+            tokio::select! {
+                job = db.get_next_job_to_execute() => {
+                    let Some(job) = job else {
+                        tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+                        continue;
+                    };
+
+                    if let Err(err) = db.update_job_started(job.id).await {
+                        warn!("Failed to mark job status to started: {:?}", err);
+                        continue;
+                    }
+
+                    let logger = JobLogger::new(db.clone(), job.id);
+                    debug!("Background job {} started, command: {}", job.id, job.command);
+                    let Ok(event) = serde_json::from_str::<BackgroundJobEvent>(&job.command) else {
+                        logkit::info!(exit_code = -1; "Failed to parse background job event, marking it as failed");
+                        continue;
+                    };
+
+                    if let Err(err) = match event {
+                        BackgroundJobEvent::SchedulerGitRepository(repository_config) => {
+                            let job = SchedulerGitJob::new(repository_config);
+                            job.run(embedding.clone()).await
+                        },
+                        BackgroundJobEvent::SyncThirdPartyRepositories(integration_id) => {
+                            let job = SyncIntegrationJob::new(integration_id);
+                            job.run(third_party_repository_service.clone()).await
+                        }
+                        BackgroundJobEvent::SchedulerGithubGitlabRepository(integration_id) => {
+                            let job = SchedulerGithubGitlabJob::new(integration_id);
+                            job.run(embedding.clone(), third_party_repository_service.clone(), integration_service.clone()).await
+                        }
+                        BackgroundJobEvent::WebCrawler(job) => {
+                            job.run(embedding.clone()).await
+                        }
+                        BackgroundJobEvent::IndexGarbageCollection => {
+                            let job = IndexGarbageCollection;
+                            job.run(repository_service.clone(), context_service.clone()).await
+                        }
+                    } {
+                        logkit::info!(exit_code = 1; "Job failed {}", err);
+                    } else {
+                        logkit::info!(exit_code = 0; "Job completed successfully");
+                    }
+                    logger.finalize().await;
+                    debug!("Background job {} completed", job.id);
+                },
+                Some(now) = hourly.next() => {
+                    if let Err(err) = DbMaintainanceJob::cron(now, context_service.clone(), db.clone()).await {
+                        warn!("Database maintainance failed: {:?}", err);
+                    }
+
+                    if let Err(err) = SchedulerGitJob::cron(now, git_repository_service.clone(), job_service.clone()).await {
+                        warn!("Scheduler job failed: {:?}", err);
+                    }
+
+                    if let Err(err) = SyncIntegrationJob::cron(now, integration_service.clone(), job_service.clone()).await {
+                        warn!("Sync integration job failed: {:?}", err);
+                    }
+
+                    if let Err(err) = SchedulerGithubGitlabJob::cron(now, third_party_repository_service.clone(), job_service.clone()).await {
+                        warn!("Index issues job failed: {err:?}");
+                    }
+
+                    if let Err(err) = IndexGarbageCollection.run(repository_service.clone(), context_service.clone()).await {
+                        warn!("Index garbage collection job failed: {err:?}");
+                    }
+
+                },
+                Some(now) = daily.next() => {
+                    if let Err(err) = LicenseCheckJob::cron(now, license_service.clone(), notification_service.clone()).await {
+                        warn!("License check job failed: {err:?}");
+                    }
+                }
+                else => {
+                    warn!("Background job channel closed");
+                    break;
+                }
+            };
+        }
+    });
+}
